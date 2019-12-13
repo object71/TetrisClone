bd4463ccacb6f946a94d7f70782d4e8330fb5a00
Hristo Stamenov <capitane71@gmail.com>
2019-12-14 00:10:52 +0200

    Section Shape and classes finished

---
+++ Assets/Scripts/Core/Spawner.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public Shape[] allShapes;
+
+    Shape GetRandomShape()
+    {
+        int i = Random.Range(0, allShapes.Length);
+
+        if (allShapes[i])
+        {
+            return allShapes[i];
+        }
+        else
+        {
+            Debug.LogWarning("Warning! Invalid shape in spawner.");
+            return null;
+        }
+    }
+
+    public Shape SpawnShape()
+    {
+        Shape shape = null;
+        shape = Instantiate(GetRandomShape(), transform.position, Quaternion.identity) as Shape;
+
+        if (shape)
+        {
+            return shape;
+        }
+        else
+        {
+            Debug.LogWarning("Warning! Invalid shape in spawner.");
+        }
+
+        return shape;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = Vector3Int.RoundToInt(transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Managers/GameController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    Board gameBoard;
+    Spawner spawner;
+
+    Shape activeShape;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameBoard = GameObject.FindObjectOfType<Board>();
+        spawner = GameObject.FindObjectOfType<Spawner>();
+
+        if (spawner)
+        {
+            if (activeShape == null)
+            {
+                activeShape = spawner.SpawnShape();
+            }
+        }
+
+        if (!gameBoard)
+        {
+            Debug.LogWarning("Warning! There is no game board!");
+        }
+
+        if (!spawner)
+        {
+            Debug.LogWarning("Warning! There is no spawner!");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!gameBoard || !spawner)
+        {
+            return;
+        }
+
+
+
+    }
+}
