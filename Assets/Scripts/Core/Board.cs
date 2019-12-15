4278f36850094ccdf21eed95226e8478203e625f
Hristo Stamenov <capitane71@gmail.com>
2019-12-15 15:03:43 +0200

    Core gameplay is finished

--- Assets/Scripts/Core/Board.cs
+++ Assets/Scripts/Core/Board.cs
@@ -28,6 +28,57 @@ public class Board : MonoBehaviour
 
     }
 
+    public bool IsValidPosition(Shape shape)
+    {
+        foreach (Transform child in shape.transform)
+        {
+            Vector2Int position = Vector2Int.RoundToInt(child.position);
+
+            if (!IsWithinBoard(position.x, position.y))
+            {
+                return false;
+            }
+
+            if (IsOccupied(position.x, position.y, shape))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public void StoreShapeInGrid(Shape shape)
+    {
+        if (shape == null)
+        {
+            return;
+        }
+
+        foreach (Transform child in shape.transform)
+        {
+            Vector2Int position = Vector2Int.RoundToInt(child.position);
+            grid[position.x, position.y] = child;
+        }
+    }
+
+    private bool IsWithinBoard(int x, int y)
+    {
+        if (x >= 0 && x < width && y >= 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private bool IsOccupied(int x, int y, Shape shape)
+    {
+        return (grid[x, y] != null && grid[x, y].parent != shape.transform);
+    }
+
     void DrawEmptyCells()
     {
         if (emptySprite)
@@ -48,5 +99,92 @@ public class Board : MonoBehaviour
         {
             Debug.Log("Warning! Assign the empty sprite object");
         }
+    }
+
+    bool IsComplete(int y)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            if (grid[x, y] == null)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    void ClearRow(int y)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            Transform parent = null;
+            parent = grid[x, y].parent;
+
+            Destroy(grid[x, y].gameObject);
+            grid[x, y] = null;
+
+            if (parent.childCount == 0)
+            {
+                Destroy(parent);
+            }
+        }
+
+    }
+
+    private void ShiftOneRowDown(int y)
+    {
+        if (y == 0)
+        {
+            return;
+        }
+
+        for (int x = 0; x < width; x++)
+        {
+            if (grid[x, y] != null)
+            {
+                grid[x, y - 1] = grid[x, y];
+                grid[x, y] = null;
+                grid[x, y - 1].position += new Vector3(0, -1, 0);
+            }
+        }
+    }
+
+    private void ShiftRowsDown(int startY)
+    {
+        for (int i = startY; i < height; i++)
+        {
+            ShiftOneRowDown(i);
+        }
+    }
+
+    public void ClearAllRows()
+    {
+        int y = 0;
+        while (y < height)
+        {
+            if (IsComplete(y))
+            {
+                ClearRow(y);
+                ShiftRowsDown(y + 1);
+            }
+            else
+            {
+                y++;
+            }
+        }
+    }
+
+    public bool IsOverLimit(Shape shape)
+    {
+        foreach (Transform child in shape.transform)
+        {
+            if (child.transform.position.y >= (height - header - 1))
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
--- Assets/Scripts/Managers/GameController.cs
+++ Assets/Scripts/Managers/GameController.cs
@@ -1,13 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
-    Board gameBoard;
-    Spawner spawner;
+    private Board gameBoard;
+    private Spawner spawner;
+    private Shape activeShape;
 
-    Shape activeShape;
+    // The interval at which the shapes drop
+    private float dropInterval = 0.75f;
+    private float timeToDrop = 0f;
+
+    // the update speed for moving left or right
+    private float leftRightKeyRepeatRate = 0.15f;
+    private float timeToNextLeftRightKey = 0f;
+
+    // the update speed for rotating
+    private float rotateKeyRepeatRate = 0.1f;
+    private float timeToNextRotateKey = 0f;
+
+    // the update speed for moving doown
+    private float downKeyRepeatRate = 0.05f;
+    private float timeToNextDownKey = 0f;
+
+    private bool gameIsOver = false;
+
+    public GameObject gameOverPanel;
 
     // Start is called before the first frame update
     void Start()
@@ -15,12 +35,9 @@ public class GameController : MonoBehaviour
         gameBoard = GameObject.FindObjectOfType<Board>();
         spawner = GameObject.FindObjectOfType<Spawner>();
 
-        if (spawner)
+        if (gameOverPanel)
         {
-            if (activeShape == null)
-            {
-                activeShape = spawner.SpawnShape();
-            }
+            gameOverPanel.SetActive(false);
         }
 
         if (!gameBoard)
@@ -32,17 +49,126 @@ public class GameController : MonoBehaviour
         {
             Debug.LogWarning("Warning! There is no spawner!");
         }
+        else
+        {
+            if (activeShape == null)
+            {
+                activeShape = spawner.SpawnShape();
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!gameBoard || !spawner)
+        if (!gameBoard || !spawner || !activeShape || gameIsOver)
         {
             return;
         }
 
+        ActOnPlayerInput();
 
+        if (Time.time > timeToDrop)
+        {
+            MoveActiveShapeDown();
+        }
 
+    }
+
+    private void MoveActiveShapeDown()
+    {
+        timeToDrop = Time.time + dropInterval;
+        if (activeShape)
+        {
+            activeShape.MoveDown();
+
+            if (!gameBoard.IsValidPosition(activeShape))
+            {
+                activeShape.MoveUp();
+
+                if (gameBoard.IsOverLimit(activeShape))
+                {
+                    GameOver();
+                }
+                else
+                {
+                    LandShape();
+                }
+            }
+        }
+    }
+
+    private void GameOver()
+    {
+        gameIsOver = true;
+
+        if (gameOverPanel)
+        {
+            gameOverPanel.SetActive(true);
+        }
+    }
+
+    private void LandShape()
+    {
+        gameBoard.StoreShapeInGrid(activeShape);
+        activeShape = spawner.SpawnShape();
+
+        timeToNextDownKey = Time.time;
+        timeToNextLeftRightKey = Time.time;
+        timeToNextRotateKey = Time.time;
+
+        gameBoard.ClearAllRows();
+    }
+
+    private void ActOnPlayerInput()
+    {
+        if (Input.GetButton("MoveRight") && Time.time > timeToNextLeftRightKey || Input.GetButtonDown("MoveRight"))
+        {
+            timeToNextLeftRightKey = Time.time + leftRightKeyRepeatRate;
+
+            activeShape.MoveRight();
+
+            if (!gameBoard.IsValidPosition(activeShape))
+            {
+                activeShape.MoveLeft();
+            }
+        }
+        else if (Input.GetButton("MoveLeft") && Time.time > timeToNextLeftRightKey || Input.GetButtonDown("MoveLeft"))
+        {
+            timeToNextLeftRightKey = Time.time + leftRightKeyRepeatRate;
+
+            activeShape.MoveLeft();
+
+            if (!gameBoard.IsValidPosition(activeShape))
+            {
+                activeShape.MoveRight();
+            }
+        }
+        else if (Input.GetButtonDown("Rotate") && Time.time > timeToNextRotateKey)
+        {
+            timeToNextRotateKey = Time.time + rotateKeyRepeatRate;
+
+            activeShape.RotateRight();
+
+            if (!gameBoard.IsValidPosition(activeShape))
+            {
+                activeShape.RotateLeft();
+            }
+        }
+        else if (Input.GetButton("MoveDown") && Time.time > timeToNextDownKey)
+        {
+            timeToNextDownKey = Time.time + downKeyRepeatRate;
+            MoveActiveShapeDown();
+        }
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+        if (gameOverPanel)
+        {
+            gameOverPanel.SetActive(false);
+        }
     }
 }
